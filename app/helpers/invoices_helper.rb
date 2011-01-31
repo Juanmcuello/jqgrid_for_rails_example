15aa1151afd5f4d09c6892ddefdd8f530bdb85a1
Juan M. Cuello <juanmacuello@gmail.com>
2011-01-30 21:16:08 -0300

    Update helper to use the new plugin version 1.0.0.

--- app/helpers/invoices_helper.rb
+++ app/helpers/invoices_helper.rb
@@ -1,23 +1,23 @@
 module InvoicesHelper
 
-  include JqgridsHelper 
+  include JqgridsHelper
 
   def invoices_jqgrid
 
     options = {:on_document_ready => true, :html_tags => false}
 
-    grid_options = {
+    grid = [{
       :url => '/invoices',
       :datatype => 'json',
       :mtype => 'GET',
       :colNames => ['Inv No','Date', 'Amount','Tax','Total','Notes'],
-      :colModel  => [ 
-        { :name => 'invid',   :index => 'invid',    :width => 55 }, 
-        { :name => 'invdate', :index => 'invdate',  :width => 90 }, 
+      :colModel  => [
+        { :name => 'invid',   :index => 'invid',    :width => 55 },
+        { :name => 'invdate', :index => 'invdate',  :width => 190 },
         { :name => 'amount',  :index => 'amount',   :width => 80,   :align => 'right' },
-        { :name => 'tax',     :index => 'tax',      :width => 80,   :align => 'right' }, 
+        { :name => 'tax',     :index => 'tax',      :width => 80,   :align => 'right' },
         { :name => 'total',   :index => 'total',    :width => 80,   :align => 'right' },
-        { :name => 'note',    :index => 'note',     :width => 150,  :sortable => false } 
+        { :name => 'note',    :index => 'note',     :width => 150,  :sortable => false }
       ],
       :pager => '#invoices_pager',
       :rowNum => 10,
@@ -27,9 +27,14 @@ module InvoicesHelper
       :viewrecords => true,
       :caption => 'My first grid',
       :onSelectRow => "function() { alert('Row selected!');}".to_json_var
-    }
+    }]
 
-    jqgrid 'invoices_list', options, grid_options 
+    pager = [:navGrid, "#invoices_pager", {:del => true }, {}, {}, {:closeOnEscape => true}]
+
+    pager_button = [:navButtonAdd, "#invoices_pager", {:caption => 'Add', :onClickButton => 'function() {alert("Custom button!")}'.to_json_var }]
+
+    jqgrid_api 'invoices_list', grid, pager, pager_button, options
+
   end
 
 end
